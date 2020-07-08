b124f02ec1dc6dc8038034ef774b99921acc8341
vogonyo <vogonyo101@gmail.com>
2020-07-08 15:00:36 +0300

    changed footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -102,7 +102,7 @@ const Footer = () => {
       </StyledSocial>
       <StyledMetadata tabindex="-1">
         <StyledGitHubLink
-          href="https://github.com/bchiang7/v4"
+          href="https://github.com/vogonyo/portfolio_project"
           target="_blank"
           rel="nofollow noopener noreferrer">
           <div>Designed &amp; Built by Victor Ogonyo</div>
